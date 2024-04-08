5d4ccf7ef90d683a69fb56065e1930f2fbc8d113
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-08 14:02:09 +0900

    [Feature] #10 - add Color

--- SafeLoad/SafeLoad/Global/Literals/ColorLiterals.swift
+++ SafeLoad/SafeLoad/Global/Literals/ColorLiterals.swift
@@ -11,8 +11,36 @@ extension UIColor {
     
     // MARK: - Gray
     
+    static var gray300: UIColor {
+        return UIColor(hex: "#6B6E82")
+    }
+    
+    static var gray400: UIColor {
+        return UIColor(hex: "#4A4D63")
+    }
+    
+    static var gray500: UIColor {
+        return UIColor(hex: "#2C2C37")
+    }
+    
+    static var gray600: UIColor {
+        return UIColor(hex: "#18171C")
+    }
+    
     static var gray800: UIColor {
         return UIColor(hex: "#464964")
+    }
+    
+    static var red400: UIColor {
+        return UIColor(hex: "#FF4F17")
+    }
+    
+    static var white000: UIColor {
+        return UIColor(hex: "#FFFFFF")
+    }
+    
+    static var black000: UIColor {
+        return UIColor(hex: "#000000")
     }
 }
     
