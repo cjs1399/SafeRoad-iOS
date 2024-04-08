87a44aab05f26fbf271fe8856626b6de946ff3b7
Cheon, Seongwoo <cjs1399@naver.com>
2024-04-08 14:01:58 +0900

    [Fix] #10 - Fix nil File erorr

---
+++ SafeLoad/SafeLoad/Presentation/Map/ViewModels/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  SafeLoad
+//
+//  Created by 천성우 on 4/8/24.
+//
+
+import Foundation
---
+++ SafeLoad/SafeLoad/Presentation/Map/Views/Components/MapComponent.swift
@@ -0,0 +1,8 @@
+//
+//  MapComponent.swift
+//  SafeLoad
+//
+//  Created by 천성우 on 4/8/24.
+//
+
+import Foundation
